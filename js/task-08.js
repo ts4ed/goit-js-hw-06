a7271ce306381d7758c8e08432a8fdd4e86677db
ts4ed <103256726+ts4ed@users.noreply.github.com>
2022-06-10 17:19:20 +0300

    update

--- js/task-08.js
+++ js/task-08.js
@@ -1,28 +1,18 @@
 const form = document.querySelector(".login-form");
-const input = document.querySelectorAll('input')
+const input = document.querySelectorAll("input");
 form.addEventListener("submit", onFormSubmit);
+let forms = [];
 
 function onFormSubmit(event) {
   event.preventDefault();
-  const formElement = event.currentTarget.elements;
-  const mail = formElement.email.value;
-  const password = formElement.password.value;
-  let forms = {
-    mail: '',
-    password: '',
-  };
-  
+
   for (var i = 0; i < input.length; i++) {
     if (!input[i].value) {
       alert(`Заполните поле  ${input[i].type}`);
     }
+    forms.push(`${input[i].type}: ${input[i].value}`);
   }
 
-  if ((mail !== "") & (password !== "")) {
-    forms.mail = `"${mail}"`;
-    forms.password = `"${password}"`;
-    console.log(forms);
-  }
-
+  console.log(forms);
   form.reset();
 }
--- js/task-09.js
+++ js/task-09.js
@@ -19,11 +19,12 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+  
 }
 
 function changeBg() {
-  bodyEl.style.backgroundColor = getRandomHexColor();
-  spanEl.textContent = getRandomHexColor();
+  let color = getRandomHexColor()
+  spanEl.textContent = color;
+  bodyEl.style.backgroundColor = color;
 }
-
 
