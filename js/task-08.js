5e86e7c3e7cd8a233d244f903995b14abde4798d
ts4ed <103256726+ts4ed@users.noreply.github.com>
2022-06-11 13:43:02 +0300

    Update

--- js/task-08.js
+++ js/task-08.js
@@ -2,17 +2,19 @@ const form = document.querySelector(".login-form");
 const input = document.querySelectorAll("input");
 form.addEventListener("submit", onFormSubmit);
 let forms = [];
-
 function onFormSubmit(event) {
   event.preventDefault();
 
-  for (var i = 0; i < input.length; i++) {
-    if (!input[i].value) {
-      alert(`Заполните поле  ${input[i].type}`);
-    }
-    forms.push(`${input[i].type}: ${input[i].value}`);
-  }
 
-  console.log(forms);
+
+    for (var i = 0; i < input.length; i++) {
+      if (!input[i].value) {
+        return alert(`Заполните все поля`);
+
+      }
+      forms.push(`${input[i].type}: ${input[i].value}`);
+    }
+
+    console.log(forms);
   form.reset();
 }
